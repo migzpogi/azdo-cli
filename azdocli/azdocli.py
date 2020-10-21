2083c2aca52cde36e32883fc23b6b166a454d480
Migz <migz.zgim@gmail.com>
2020-10-21 11:29:13 +0800

    Uses nested commands

--- azdocli/azdocli.py
+++ azdocli/azdocli.py
@@ -1,10 +1,24 @@
 import click
 
+
 def foo():
     return True
 
 
-@click.command()
+@click.group()
 def cli():
-    """Example script"""
-    click.echo("Hello World!")
+    pass
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    for x in range(count):
+        click.echo(f'Hello {name}!')
+
+
+cli.add_command(hello)
+
+
