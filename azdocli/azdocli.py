4835290aa46f49b7653e35dc0a858a465d829d34
Migz <migz.zgim@gmail.com>
2020-10-26 11:17:37 +0800

    Adds config initialization and loading

--- azdocli/azdocli.py
+++ azdocli/azdocli.py
@@ -1,4 +1,5 @@
 import click
+import configparser
 
 
 def foo():
@@ -19,6 +20,30 @@ def hello(count, name):
         click.echo(f'Hello {name}!')
 
 
+@click.command()
+def stub():
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    click.echo(f'Config {config["org"]["pat"]}')
+
+
+@click.command()
+@click.option('--org_name', prompt='The name of the Azure DevOps organization',
+              help='The name of the Azure Devops organization')
+@click.option('--pat', prompt='Your personal access token for that organization',
+              help='Your personal access token for that organization')
+def init(org_name, pat):
+    config = configparser.ConfigParser()
+    config['org'] = {
+        'org_name': org_name,
+        'pat': pat
+    }
+    with open('settings.ini', 'w') as f:
+        config.write(f)
+
+
 cli.add_command(hello)
+cli.add_command(stub)
+cli.add_command(init)
 
 
