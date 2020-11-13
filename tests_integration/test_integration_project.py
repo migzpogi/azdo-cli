c2f1a13a1af1b40804b1b1d502d35cb991f8edd0
Migz <migz.zgim@gmail.com>
2020-11-13 14:47:58 +0800

    Adds proper integration test for getall projects

--- tests_integration/test_integration_project.py
+++ tests_integration/test_integration_project.py
@@ -1,11 +1,19 @@
 import unittest
+import json
 
 from click.testing import CliRunner
+from azdocli.azdocli import cli
 
 
 class TestIntegrationProject(unittest.TestCase):
-    def test_foobar(self):
-        self.assertTrue(True)
+    def test_if_getall_projects_then_count_must_be_two(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['projects', 'getall'])
+
+        actual = json.loads(result.output.replace("'", '"'))
+
+        self.assertEqual(2, actual['count'])
+        self.assertEqual(result.exit_code, 0)
 
 
 if __name__ == '__main__':
