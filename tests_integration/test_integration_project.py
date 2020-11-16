b9b674ee5e63ff0c8fe34200bcbe0853520ad9c3
Migz <migz.zgim@gmail.com>
2020-11-16 10:57:34 +0800

    Adds integration test for get project

--- tests_integration/test_integration_project.py
+++ tests_integration/test_integration_project.py
@@ -15,6 +15,18 @@ class TestIntegrationProject(unittest.TestCase):
         self.assertEqual(2, actual['count'])
         self.assertEqual(result.exit_code, 0)
 
+    def test_if_get_project_then_id_must_be_correct(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['projects',
+                                     'get',
+                                     '--name',
+                                     'leonhard'])
+
+        actual = json.loads(result.output.replace("'", '"'))
+
+        self.assertEqual('leonhard', actual['name'])
+        self.assertEqual(result.exit_code, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
