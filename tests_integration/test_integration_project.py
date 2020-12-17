a8bc476b1369e731f194f9b99ef9b575049e19be
Migz <migz.zgim@gmail.com>
2020-12-17 23:36:06 +0800

    Tries assert exit code, but fails

--- tests_integration/test_integration_project.py
+++ tests_integration/test_integration_project.py
@@ -4,9 +4,12 @@ import json
 from click.testing import CliRunner
 from azdocli.azdocli import cli
 
+from azure.devops.exceptions import AzureDevOpsServiceError, AzureDevOpsAuthenticationError
+
 
 class TestIntegrationProject(unittest.TestCase):
     def test_if_getall_projects_then_count_must_be_two(self):
+        """001"""
         runner = CliRunner()
         result = runner.invoke(cli, ['projects', 'getall'])
 
@@ -16,6 +19,7 @@ class TestIntegrationProject(unittest.TestCase):
         self.assertEqual(result.exit_code, 0)
 
     def test_if_get_project_then_id_must_be_correct(self):
+        """002"""
         runner = CliRunner()
         result = runner.invoke(cli, ['projects',
                                      'get',
@@ -26,6 +30,27 @@ class TestIntegrationProject(unittest.TestCase):
 
         self.assertEqual('leonhard', actual['name'])
         self.assertEqual(result.exit_code, 0)
+
+    def test_foo(self):
+        filename = './tests_integration/settings-wrong-creds.ini'
+        runner = CliRunner()
+        result = runner.invoke(cli, ['projects',
+                                     'get',
+                                     '--name',
+                                     'leonhard',
+                                     '--filename',
+                                     filename])
+        self.assertEqual(result.exit_code, 0)
+        # with self.assertRaises(AzureDevOpsServiceError):
+        #     result = runner.invoke(cli, ['projects',
+        #                                  'get',
+        #                                  '--name',
+        #                                  'leonhard',
+        #                                  '--filename',
+        #                                  filename])
+        # with self.assertRaises(AzureDevOpsServiceError):
+        #     runner.invoke(cli, ['projects', 'getall'])
+
 
 
 if __name__ == '__main__':
