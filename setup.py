22b0c6a5fcc91fae094127d95230b0fa59b868a2
Migz <migz.zgim@gmail.com>
2021-02-06 12:34:58 +0800

    Bumps to 0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="azdo-cli",
-    version="0.0.9",
+    version="0.0.10",
     author="Migz Estrella",
     author_email="me@migzestrella.com",
     description="An Azure DevOps CLI client.",
